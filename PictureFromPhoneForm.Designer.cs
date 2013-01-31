52b31551dbc1ce6d8de248ae3e46957a2d5684fc
Tomasz Janczuk <tomasz@janczuk.org>
2013-01-31 14:53:10 -0800

    finalize product, add setup

--- PictureFromPhoneForm.Designer.cs
+++ PictureFromPhoneForm.Designer.cs
@@ -29,34 +29,54 @@
         private void InitializeComponent()
         {
             this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.labelStatus = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // webBrowser
             // 
             this.webBrowser.AllowWebBrowserDrop = false;
             this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.IsWebBrowserContextMenuEnabled = false;
             this.webBrowser.Location = new System.Drawing.Point(0, 0);
             this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser.Name = "webBrowser";
+            this.webBrowser.ScriptErrorsSuppressed = true;
             this.webBrowser.ScrollBarsEnabled = false;
-            this.webBrowser.Size = new System.Drawing.Size(700, 530);
+            this.webBrowser.Size = new System.Drawing.Size(724, 530);
             this.webBrowser.TabIndex = 0;
             this.webBrowser.Url = new System.Uri("http://surla.mobi/upload/livewriter", System.UriKind.Absolute);
+            this.webBrowser.Visible = false;
             this.webBrowser.WebBrowserShortcutsEnabled = false;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelStatus.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.Location = new System.Drawing.Point(0, 0);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(118, 24);
+            this.labelStatus.TabIndex = 1;
+            this.labelStatus.Text = "Initializing...";
+            this.labelStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // PictureFromPhoneForm
             // 
-            this.ClientSize = new System.Drawing.Size(700, 530);
+            this.ClientSize = new System.Drawing.Size(724, 530);
+            this.Controls.Add(this.labelStatus);
             this.Controls.Add(this.webBrowser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "PictureFromPhoneForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Insert picture from phone";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Label labelStatus;
     }
 }
--- PictureFromPhoneForm.cs
+++ PictureFromPhoneForm.cs
@@ -21,12 +21,40 @@ namespace Surla.Mobi.PictureFromPhone
             InitializeComponent();
 
             this.webBrowser.ObjectForScripting = this;
+            this.webBrowser.DocumentCompleted += webBrowser_DocumentCompleted;
         }
 
-        public void PictureUploaded(string uri)
+        void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(this.webBrowser.DocumentText) 
+                && this.webBrowser.DocumentText.Contains("This surla.mobi page has loaded")) {
+                this.labelStatus.Visible = false;
+                this.webBrowser.Visible = true;
+            }
+            else 
+            {
+                this.labelStatus.Text = "Error accessing http://surla.mobi service, try again later.";
+                this.labelStatus.PerformLayout();
+            }
+        }
+
+        protected override void OnLayout(LayoutEventArgs levent)
+        {
+ 	        base.OnLayout(levent);
+
+            this.labelStatus.Location = new Point(
+                (this.ClientSize.Width - this.labelStatus.Width) / 2,
+                (this.ClientSize.Height - this.labelStatus.Height) / 2);
+        }
+        
+        public void PictureUploaded(string uri, string contentType)
         {
             this.uri = uri;
             this.Close();
+        }
+
+        public void UploadError(string id)
+        {
         }
     }
 }
